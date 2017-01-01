0947e72e2ec7f1c7155a6440623044e58b1449ff
Rocketman <rocketman@Davids-MacBook-Pro.local>
2017-01-01 07:56:51 -0500

    First version of Programming Assignment 2 - includes a function to test the required work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ###############################################################
+## The following functions implement a means to reduce the cost of 
+## computing the inverse of a large unchanging matrix.
+## ###############################################################
 
-## Write a short comment describing this function
+## makeCacheMatrix defines a 'class' which stores an invertable
+## matrix and it's inverted value. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverted <- function(inverted) i <<- inverted
+  getinverted <- function() i
+  list(set = set, get = get, setinverted = setinverted, getinverted = getinverted)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses the solve function to invert the matrix stored in an 
+## cachedMatrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverted()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverted(i)
+  i
 }
+
+
+## testProgrammingAssignment2 is a very simple test of the makeCacheMatrix
+## and the cacheSolve functions 
+
+testProgrammingAssignment2 <- function() {
+  message("======TESTING======")
+  A = matrix(c(1,2,3,1), nrow = 2, ncol = 2)
+  message("------A-----")
+  print(A)
+  
+  cA = makeCacheMatrix(A)
+  message("------cA-----")
+  print(cA)
+  
+  sA = cacheSolve(cA)
+  message("------sA-------")
+  print(sA)
+  
+  sA1 = cacheSolve(cA)
+  message("------sA1------")
+  print(sA1)
+  
+  cB = makeCacheMatrix(A)
+  message("------cB------")
+  print(cB)
+  
+  sB = cacheSolve(cB)
+  message("------sB------")
+  print(sB)
+}
+
+# testProgrammingAssignment2()
+
+
